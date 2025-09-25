b963009cb8589016bc773afd519872576167f8c9
phfr24 <phfr24@inf.ufpr.br>
2025-09-25 12:09:35 -0300

    manage minors with xarray

--- src/sw/kmod/chrdev.c
+++ src/sw/kmod/chrdev.c
@@ -2,26 +2,55 @@
 #include "data.h"
 #include "hw/cafe.h"
 
+#define MINORS_XA_LIMIT XA_LIMIT(0, 255)
+
 static struct chrdev_ctrl {
-    struct class *class;
+    struct class *chrdev_class;
     struct file_operations fops;
     int major;
+    struct xarray minors_xa;
+    struct xa_limit minors_lim;
 } ctrl;
+
+static int allocate_minor(struct pci_dev *pdev) {
+    struct cafe_dev_data *data;
+    int minor;
+    int err;
+
+    data = pci_get_drvdata(pdev);
+
+    if (!(err = xa_alloc(&ctrl.minors_xa, &minor, pdev, ctrl.minors_lim,
+                    GFP_KERNEL)))
+        data->dev_num = MKDEV(ctrl.major, minor);
+
+    return err;
+}
+
+static void free_minor(struct pci_dev *pdev) {
+    struct cafe_dev_data *data;
+
+    data = pci_get_drvdata(pdev);
+    xa_erase(&ctrl.minors_xa, MINOR(data->dev_num));
+}
+
+static struct pci_dev *cafe_minor_to_dev(int minor) {
+    return xa_load(&ctrl.minors_xa, minor);
+}
 
 int cafe_chrdev_create(struct pci_dev *pdev) {
     struct cafe_dev_data *data;
     struct device *dev;
+    int err;
 
     dev = &pdev->dev;
+    data = pci_get_drvdata(pdev);
 
-    if (!(data = pci_get_drvdata(pdev))) {
-        dev_err (dev, "pci_get_drvdata() returned NULL!");
-        return -ENOMEM;
+    if (!(err = allocate_minor(pdev))) {
+        dev_err (dev, "allocate_minor() failed: %d\n", err);
+        return err;
     }
 
-    data->dev_num = MKDEV(ctrl.major, 0/* minor */);
-
-    data->chrdev = device_create(ctrl.class, dev, data->dev_num, NULL,
+    data->chrdev = device_create(ctrl.chrdev_class, dev, data->dev_num, NULL,
             CAFE_HW_NAME);
 
     if (IS_ERR(data->chrdev)) {
@@ -36,7 +65,7 @@ void cafe_chrdev_destroy(struct pci_dev *pdev) {
     struct cafe_dev_data *data;
 
     data = pci_get_drvdata(pdev);
-    device_destroy(ctrl.class, data->dev_num);
+    device_destroy(ctrl.chrdev_class, data->dev_num);
 }
 
 int cafe_chrdev_init(void) {
@@ -49,17 +78,22 @@ int cafe_chrdev_init(void) {
         return ctrl.major;
     }
 
-    ctrl.class = class_create(CAFE_HW_NAME);
+    ctrl.chrdev_class = class_create(CAFE_HW_NAME);
 
-    if (IS_ERR(ctrl.class)) {
+    if (IS_ERR(ctrl.chrdev_class)) {
         pr_err("class_create() failed!\n");
-        return PTR_ERR(ctrl.class);
+        return PTR_ERR(ctrl.chrdev_class);
     }
+
+    xa_init(&ctrl.minors_xa);
+    ctrl.minors_lim = MINORS_XA_LIMIT;
 
     return 0;
 }
 
 void cafe_chrdev_deinit(void) {
-    class_destroy(ctrl.class);
+    class_destroy(ctrl.chrdev_class);
     unregister_chrdev(ctrl.major, CAFE_HW_NAME);
+    /* do i need to unregister all minor devices? */
+    xa_destroy(&ctrl.minors_xa);
 }
