6769b027b165128f6ab7d79f362bd776262458c8
phfr24 <phfr24@inf.ufpr.br>
2025-09-27 00:10:22 -0300

    improve error handling on fops.c

--- src/sw/kmod/fops.c
+++ src/sw/kmod/fops.c
@@ -11,8 +11,10 @@ int cafe_mmap(struct file *f, struct vm_area_struct *vma) {
     int err;
 
     if (!(pdev = cafe_minor_to_dev(iminor(f->f_inode)))) {
-        pr_err("cafe_minor_to_dev() returned NULL\n");
-        return -EIO;
+        pr_err("cafe_minor_to_dev() returned NULL! minor does not correspond"
+                "to any device\n");
+        err = -ENODEV;
+        goto err_exit;
     }
 
     dev = &pdev->dev;
@@ -20,13 +22,19 @@ int cafe_mmap(struct file *f, struct vm_area_struct *vma) {
 
     if ((len = vma->vm_end - vma->vm_start) > data->bar.len) {
         dev_err(dev, "cafe_mmap() out of bounds request\n");
-        return -EIO;
+        err = -EINVAL;
+        goto err_exit;
     }
 
     if ((err = vm_iomap_memory(vma, data->bar.start, len))) {
-        dev_err(dev, "vm_iomap_memory() failed: %d\n", err);
-        return err;
+        dev_err(dev, "vm_iomap_memory() failed: %pe\n", ERR_PTR(err));
+        goto err_exit;
     }
 
     return 0;
+
+err_exit:
+
+    pr_err("cafe_mmap() failed: %pe\n", ERR_PTR(err));
+    return err;
 }
