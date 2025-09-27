b41bf61937564adcc7c23dc4119b1bd50c95e5b5
phfr24 <phfr24@inf.ufpr.br>
2025-09-27 18:26:59 -0300

    fix usermode test program

--- src/sw/user/test_mmap.c
+++ src/sw/user/test_mmap.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <stdint.h>
 
-int main() {
-  int fd;
+int main(int argc, char **argv) {
   uint64_t *mem;
+  int fd;
+  int count;
 
   if ((fd = open("/dev/cafe", O_RDWR)) < 0) {
-    fprintf(stderr, "open falhou\n");
+    perror("failed to open /dev/cafe\n");
     return 1;
   }
 
-  mem = mmap(NULL, 8, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-
-  if (mem == MAP_FAILED) {
-    fprintf(stderr, "mmap falhou\n");
+  if ((mem = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) ==
+      MAP_FAILED) {
+    perror("mmap failed\n");
     return 1;
   }
 
-  for (int i = 0; i < (4096 / 8) ; i++) {
+  if (argc < 2)
+    count = 1;
+  else
+    count = atoi(argv[1]);
+
+  if (count < 1)
+    count = 4096 / 8;
+
+  for (int i = 0; i < count; i++)
     mem[i] = 0xcafecafe;
-
-    if (mem[i] != 0xcafecafe)
-      fprintf(stderr, "got %x\n", mem[i]);
-  }
 
   return 0;
 }
