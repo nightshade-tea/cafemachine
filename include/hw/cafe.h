53197ebd3bdbd9d37d6b7a2da7e93c2a70501124
phfr24 <phfr24@inf.ufpr.br>
2025-09-30 14:31:53 -0300

    update

--- include/hw/cafe.h
+++ include/hw/cafe.h
@@ -1,6 +1,8 @@
 #ifndef CAFE_H
 #define CAFE_H
 
+#include <stddef.h>
+
 #define CAFE_HW_NAME "cafe"
 #define CAFE_VENDOR_ID 0x1ced
 #define CAFE_DEVICE_ID 0xcafe
---
+++ src/sw/user/test_dma.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include "hw/cafe.h"
+
+int main() {
+  uint64_t *mem;
+  int fd;
+
+  if ((fd = open("/dev/cafe", O_RDWR)) < 0) {
+    perror("failed to open /dev/cafe\n");
+    return 1;
+  }
+
+  if ((mem = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) ==
+      MAP_FAILED) {
+    perror("mmap failed\n");
+    return 1;
+  }
+
+  return 0;
+}
