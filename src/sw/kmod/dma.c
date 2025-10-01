34e4c01617a0e31baa59dbb92a2ce8e243c0d239
phfr24 <phfr24@inf.ufpr.br>
2025-10-01 13:22:05 -0300

    test dma write

--- src/sw/kmod/dma.c
+++ src/sw/kmod/dma.c
@@ -15,10 +15,18 @@ int cafe_dma_enable(struct pci_dev *pdev) {
 
     buf = kzalloc(512, GFP_KERNEL);
 
-    for (int i = 0 ; i < 512 ; i += 2) {
+    for (int i = 0 ; i < 256 ; i += 2) {
         buf[i] = 0xca;
         buf[i + 1] = 0xfe;
     }
+
+    writeq(256, data->bar.mmio + CAFE_DMA_SZ * 8);
+    writeq(virt_to_phys(buf), data->bar.mmio + CAFE_DMA_SRC * 8);
+    writeq(CAFE_DMA_READ, data->bar.mmio + CAFE_CMD * 8);
+
+    writeq(256, data->bar.mmio + CAFE_DMA_SZ * 8);
+    writeq(virt_to_phys(buf + 256), data->bar.mmio + CAFE_DMA_DST * 8);
+    writeq(CAFE_DMA_WRITE, data->bar.mmio + CAFE_CMD * 8);
 
     writeq(512, data->bar.mmio + CAFE_DMA_SZ * 8);
     writeq(virt_to_phys(buf), data->bar.mmio + CAFE_DMA_SRC * 8);
